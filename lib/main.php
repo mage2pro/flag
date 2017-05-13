3309d3dea0cc30c517099a7dc8cc7d36887a260f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-05-13 20:01:02 +0300

    1.0.2

--- lib/main.php
+++ lib/main.php
@@ -8,6 +8,9 @@ function df_flag_css() {return df_link_inline(df_asset_name('main', 'Df_Flag', '
 /**
  * 2017-05-13
  * @param string $country
+ * @param string $content [optional]
  * @return string
  */
-function df_flag_span($country) {return df_tag('span', 'flag-icon flag-icon-' . strtolower($country));}
+function df_flag_span($country, $content = null) {return df_tag(
+	'span', 'flag-icon flag-icon-' . strtolower($country), $content
+);}
