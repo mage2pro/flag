6ee7959737046db0f05ec344ef840c49f92c1c2f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-05-13 20:33:01 +0300

    no message

--- lib/main.php
+++ lib/main.php
@@ -1,12 +1,14 @@
 <?php
 /**
  * 2017-05-13
+ * @used-by https://github.com/mage2pro/portal-stripe/blob/0.3.9/view/frontend/templates/page/customers.phtml#L32
  * @return string
  */
 function df_flag_css() {return df_link_inline(df_asset_name('main', 'Df_Flag', 'css'));}
 
 /**
  * 2017-05-13
+ * @used-by https://github.com/mage2pro/portal-stripe/blob/0.3.9/view/frontend/templates/page/customers.phtml#L27
  * @param string $country
  * @param string $content [optional]
  * @return string
