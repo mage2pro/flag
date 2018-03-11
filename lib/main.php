643bd62b0ab7cc307ffed8e6eb88aacc862055fa
Dmitry Fedyuk <dfediuk@gmail.com>
2018-03-11 22:50:25 +0600

    1.0.4

--- lib/main.php
+++ lib/main.php
@@ -9,10 +9,10 @@ function df_flag_link() {return df_link_inline(df_asset_name(null, 'Df_Flag', 'c
 /**
  * 2017-05-13
  * @used-by https://github.com/mage2pro/portal-stripe/blob/0.3.9/view/frontend/templates/page/customers.phtml#L27
- * @param string $country
+ * @param string $iso2
  * @param string $content [optional]
  * @return string
  */
-function df_flag_span($country, $content = null) {return df_tag(
-	'span', 'flag-icon flag-icon-' . strtolower($country), $content
+function df_flag_span($iso2, $content = null) {return df_tag(
+	'span', 'flag-icon flag-icon-' . strtolower('uk' === $iso2 ? 'gb' : $iso2), $content
 );}
