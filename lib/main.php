92a458aa13e93804d57db350a73a48ed60499b7d
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-17 20:18:14 +0600

    no message

--- lib/main.php
+++ lib/main.php
@@ -9,6 +9,8 @@ function df_flag_link() {return df_link_inline(df_asset_name(null, 'Df_Flag', 'c
 /**
  * 2017-05-13
  * @used-by https://github.com/mage2pro/portal-stripe/blob/0.3.9/view/frontend/templates/page/customers.phtml#L27
+ * @used-by https://github.com/frugue/store/blob/1.0.0/view/frontend/templates/switcher.phtml#L19
+ * @used-by https://github.com/frugue/store/blob/1.0.0/view/frontend/templates/switcher.phtml#L29
  * @param string $iso2
  * @param string $content [optional]
  * @return string
